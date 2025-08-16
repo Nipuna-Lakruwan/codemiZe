392418877bfd52fb86d2706f6ab2a327b600904c
JanithPerera <janithper9@gmail.com>
2025-08-16 23:41:22 +0530

    feat: add upload functionality for Route Seekers network design files

--- backend/routes/Games/routeSeekers.route.js
+++ backend/routes/Games/routeSeekers.route.js
@@ -5,14 +5,17 @@ import {
     getallstudentanswers, 
     updateStudentAnswers, 
     deleteAllStudentAnswers, 
-    updateAnswerStatus
+    updateAnswerStatus,
+    uploadNetworkDesign
 } from "../../controllers/Games/routeSeekers.controller.js";
 import { protect } from "../../middleware/authMiddleware.js";
+import { routeSeekersNetworkDesignUpload } from "../../middleware/uploadMiddleware.js";
 
 const router = express.Router();
 
 router.get("/questions", protect, getQuestions);
 router.post("/submit", protect, submitAnswers);
+router.post("/upload-network-design", protect, routeSeekersNetworkDesignUpload.single('file'), uploadNetworkDesign);
 router.get("/all-student-answers", protect, getallstudentanswers);
 router.put("/answers/:id", protect, updateStudentAnswers);
 router.patch("/answers/:submissionId/questions/:questionId", protect, updateAnswerStatus);
