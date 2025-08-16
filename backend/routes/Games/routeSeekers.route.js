79a869fd7da8bdb9a352460324591eb16ee60611
JanithPerera <janithper9@gmail.com>
2025-08-17 00:16:21 +0530

    feat: enhance network design functionality with new routes for retrieval, deletion, and download

--- backend/routes/Games/routeSeekers.route.js
+++ backend/routes/Games/routeSeekers.route.js
@@ -6,7 +6,10 @@ import {
     updateStudentAnswers, 
     deleteAllStudentAnswers, 
     updateAnswerStatus,
-    uploadNetworkDesign
+    uploadNetworkDesign,
+    getAllNetworkDesigns,
+    deleteNetworkDesign,
+    downloadAllNetworkDesigns
 } from "../../controllers/Games/routeSeekers.controller.js";
 import { protect } from "../../middleware/authMiddleware.js";
 import { routeSeekersNetworkDesignUpload } from "../../middleware/uploadMiddleware.js";
@@ -16,6 +19,9 @@ const router = express.Router();
 router.get("/questions", protect, getQuestions);
 router.post("/submit", protect, submitAnswers);
 router.post("/upload-network-design", protect, routeSeekersNetworkDesignUpload.single('file'), uploadNetworkDesign);
+router.get("/network-designs", protect, getAllNetworkDesigns);
+router.delete("/network-designs/:id", protect, deleteNetworkDesign);
+router.get("/download-all-network-designs", protect, downloadAllNetworkDesigns);
 router.get("/all-student-answers", protect, getallstudentanswers);
 router.put("/answers/:id", protect, updateStudentAnswers);
 router.patch("/answers/:submissionId/questions/:questionId", protect, updateAnswerStatus);
