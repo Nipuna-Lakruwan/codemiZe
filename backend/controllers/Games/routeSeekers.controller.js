8d27e3fdb075ce08c8a1a3c50128b1fc4ad18da6
JanithPerera <janithper9@gmail.com>
2025-08-18 04:19:02 +0530

    route seekers judge marks average is divived by 2

--- backend/controllers/Games/routeSeekers.controller.js
+++ backend/controllers/Games/routeSeekers.controller.js
@@ -28,7 +28,7 @@ const calculateAndUpdateRouteSeekersScore = async (schoolId) => {
         }
 
         // 3. Calculate total and update school
-        const totalScore = quizScore + averageDesignScore;
+        const totalScore = quizScore + (averageDesignScore / 2);
         await School.findByIdAndUpdate(schoolId, {
             $set: { "score.RouteSeekers": totalScore },
         });
